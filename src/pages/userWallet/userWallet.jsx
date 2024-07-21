4cdc8747322d5284380dd0f009dbe257765cca83
Mesquita Oliveira <ismael.mesquita.100@gmail.com>
2024-07-21 19:33:18 -0300

    🐛 fix: update api url

--- src/pages/userWallet/userWallet.jsx
+++ src/pages/userWallet/userWallet.jsx
@@ -17,7 +17,7 @@ const UserWallet = () => {
       ) {
         try {
           const response = await axios.get(
-            `http://localhost:3000/api/tokens/${walletClient.account.address}`
+            `https://api-backend-theta.vercel.app/${walletClient.account.address}`
           );
           if (
             response.data &&
